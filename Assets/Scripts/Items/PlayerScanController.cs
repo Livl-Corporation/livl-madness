b519dff4555be30d68e1e27dbd98519a1deb1b90
Franck G <gutmann.franck@outlook.fr>
2023-04-26 14:31:22 +0200

    feat: add scan success & fail sounds

--- Assets/Scripts/Items/PlayerScanController.cs
+++ Assets/Scripts/Items/PlayerScanController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScanController : MonoBehaviour
@@ -7,12 +9,19 @@ public class PlayerScanController : MonoBehaviour
     [SerializeField] private PlayerScore playerScore;
     [SerializeField] private ScanListController scanListController;
 
+    [Header("Sounds")]
+    [SerializeField] private AudioClip successSound;
+    [SerializeField] private AudioClip failSound;
+
+    [Header("Conig")] [SerializeField] private float scanSoundDelay = 0.2f;
+    
     public void Start()
     {
         if (scanListController == null)
         {
             scanListController = FindObjectOfType<ScanListController>();
         }
+        
     }
 
     public bool Scan(GameObject item)
@@ -20,14 +29,24 @@ public class PlayerScanController : MonoBehaviour
         
         if (!scanListController.CanBeScanned(item.name))
         {
+            var coroutine = DelayedSound(failSound);
+            StartCoroutine(coroutine);
             return false;
         }
         
-        scanListController.ScanArticle(item.name);
+        var coroutine2 = DelayedSound(successSound);
+        StartCoroutine(coroutine2);
         
+        scanListController.ScanArticle(item.name);
         playerScore.Increment(1);
         
         return true;
+    }
+
+    public IEnumerator DelayedSound(AudioClip clip)
+    {
+        yield return new WaitForSeconds(scanSoundDelay);
+        AudioSource.PlayClipAtPoint(clip, transform.position);
     }
 
 }
