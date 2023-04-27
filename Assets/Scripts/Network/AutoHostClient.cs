cd898095a984d94bf077b42ed3e491e579852d62
Tanguy Gimenez <gimeneztanguy@gmail.com>
2023-04-27 13:21:49 +0200

    feat(network): adding autoconnect

---
+++ Assets/Scripts/Network/AutoHostClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+namespace Network
+{
+    public class AutoHostClient : MonoBehaviour
+    {
+        
+        [SerializeField] NetworkManager networkManager;
+
+        private void Start()
+        {
+            if (Application.isBatchMode) // headless build
+            {
+                Debug.Log("=== Server build ===");
+                return;
+            }
+            
+            Debug.Log("=== Client build ===");
+            networkManager.StartClient();
+        }
+
+        public void JoinLocal()
+        {
+            networkManager.networkAddress = "localhost";
+            networkManager.StartClient();
+        }
+    }
+}
