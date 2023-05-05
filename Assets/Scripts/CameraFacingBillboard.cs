c1572750eb535116405ef504ce2b80b423f8536c
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-05-05 15:34:48 +0200

    fix: canvas rotation not working on clients

--- Assets/Scripts/CameraFacingBillboard.cs
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -2,16 +2,10 @@ using UnityEngine;
 
 public class CameraFacingBillboard : MonoBehaviour
 {
-    private Camera mainCamera;
-
-    private void Start()
-    {
-        mainCamera = Camera.main;
-    }
-
     void Update()
     {
-        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward,
-            mainCamera.transform.rotation * Vector3.up);
+        Camera cam = Camera.main;
+
+        transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward, cam.transform.rotation * Vector3.up);
     }
 }
