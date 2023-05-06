955fb1546927ed622caf2dae94c7220066b79b5b
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-05-06 12:55:33 +0200

    feat: update new implementation with observable to retrieve player stats to the Leaderboard.cs

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -1,16 +1,94 @@
+using System.Collections.Generic;
+using System.Linq;
+using Interfaces;
+using Models;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Leaderboard : MonoBehaviour
+public class Leaderboard : MonoBehaviour, IPlayerStatsObserver
 {
     [SerializeField] private GameObject playerScoreBoardItem;
-    [SerializeField] private Transform playerScoreBoardList;
+    [SerializeField] private Transform transformPlayerScoreBoardList;
     [SerializeField] public Sprite[] playerImages;
     [SerializeField] private VerticalLayoutGroup verticalLayoutGroup;
+    [SerializeField] private int maxPlayersToDisplayScore = 3;
     
-    private void OnEnable()
+    private PlayerStatsController _playerStatsController;
+    private readonly Dictionary<string, PlayerScoreBoardItem> _playerStatItems = new Dictionary<string, PlayerScoreBoardItem>();
+    
+    private void Start()
     {
+        // Subscribe to the PlayerStatsController
+        _playerStatsController = FindObjectOfType<PlayerStatsController>();
+        if (_playerStatsController == null)
+        {
+            Debug.LogError("PlayerStatsListController: PlayerStatsController not found");
+            return;
+        }
+        _playerStatsController.AddObserver(this);
+    }
+    
+    public void UpdatePlayerStats(Dictionary<string, PlayerStat> playerStats)
+    {
+        var missingKeys = new List<string>(_playerStatItems.Keys);
+        var orderedKeys = playerStats.Keys
+            .OrderByDescending(key => playerStats[key].Score)
+            .Select(a => a)
+            .ToList();
+        
+        // order the PlayerStat list by score
+        playerStats = playerStats.OrderByDescending(x => x.Value.Score).ToDictionary(x => x.Key, x => x.Value);
+        
+        // For each connected player
+        var index = 0;
+        foreach (var playerStat in playerStats)
+        {
+            if (!_playerStatItems.ContainsKey(playerStat.Key))
+            {
+                // If player is not displayed, add it
+                AddPlayerStat(playerStat.Value.Username);
+            }
+            
+            if(index > maxPlayersToDisplayScore)
+                return;
+            
+            var controller = _playerStatItems[playerStat.Key];
+            controller.Set(playerStat.Value, playerImages[index++]);
+            transformPlayerScoreBoardList.SetSiblingIndex(orderedKeys.IndexOf(playerStat.Key));
+            
+            missingKeys.Remove(playerStat.Key);
+        }
 
+        // Remove disconnected players from list
+        foreach (var missingKey in missingKeys)
+        {
+            RemovePlayerStat(missingKey);
+        }
+        
+    }
+
+    private void AddPlayerStat(string playerName)
+    {
+        var playerStatItem = Instantiate(playerScoreBoardItem, transformPlayerScoreBoardList);
+        _playerStatItems.Add(playerName, playerStatItem.GetComponent<PlayerScoreBoardItem>());
+    }
+
+    private void RemovePlayerStat(string playerName)
+    {
+        var playerStatItem = _playerStatItems[playerName];
+        _playerStatItems.Remove(playerName);
+        Destroy(playerStatItem.gameObject);
+    }    
+    
+    private void OnDestroy()
+    {
+        if (_playerStatsController == null) return;
+        _playerStatsController.RemoveObserver(this);
+        
+        foreach(Transform child in transformPlayerScoreBoardList)
+        {
+            Destroy(child.gameObject);
+        }
     }
 
     private int GetSpacingVerticalLayout(Player[] players)
@@ -19,13 +97,5 @@ public class Leaderboard : MonoBehaviour
         if (players.Length <= 2)
             spacing = -150;
         return spacing;
-    }
-    
-    private void OnDisable()
-    {
-        foreach(Transform child in playerScoreBoardList)
-        {
-            Destroy(child.gameObject);
-        }
     }
 }
--- Assets/Scripts/ViewControllers/PlayerScoreBoardItem.cs
+++ Assets/Scripts/ViewControllers/PlayerScoreBoardItem.cs
@@ -1,18 +1,18 @@
+using Models;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerScoreBoardItem : MonoBehaviour
+public class PlayerScoreBoardItem : MonoBehaviour 
 {
     [SerializeField] public Image playerImage;
     [SerializeField] private TMP_Text playerNameText;
     [SerializeField] private TMP_Text playerScoreText;
     
-    public void Setup(Player player, Sprite sprite)
+    public void Set(PlayerStat player, Sprite sprite)
     {
-        playerNameText.text = player.name;
-        // TODO : Pistache
-        // playerScoreText.text = player.GetScore().ToString();
+        playerNameText.text = player.Username;
+        playerScoreText.text = player.Score.ToString();
         playerImage.sprite = sprite;
     }
 }
