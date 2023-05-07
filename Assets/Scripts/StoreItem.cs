63e4312a9181c47c9f0316996c771b474b4cdc1b
Raffael Di Pietro <67447144+Augustin68@users.noreply.github.com>
2023-05-07 18:54:54 +0200

    Update Assets/Scripts/StoreItem.cs

--- Assets/Scripts/StoreItem.cs
+++ Assets/Scripts/StoreItem.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class StoreItem : MonoBehaviour
 {
-    //[SerializeField]
-    //[Tooltip("The id of the item")]
-    //public int id;
 
     [SerializeField]
     [Tooltip("The name that will be displayed in the game")]
