79a79d44f710e450360b371af59e354254fb3b60
Tanguy Gimenez <gimeneztanguy@gmail.com>
2023-05-17 23:38:15 +0200

    fix: crash

--- Assets/Scripts/ChatBehaviour.cs
+++ Assets/Scripts/ChatBehaviour.cs
@@ -69,6 +69,9 @@ public class ChatBehaviour : NetworkBehaviour
      */
     private void ShowChatPanel()
     {
+        if (chatPanel == null)
+            return;
+        
         var canvasGroup = chatPanel.GetComponent<CanvasGroup>();
             
         if (!canvasGroup) return;
