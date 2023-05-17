7e5be40885a16598cc839261db86bd0ab9383b7e
Tanguy Gimenez <gimeneztanguy@gmail.com>
2023-05-17 15:18:38 +0200

    feat: implement our own networkmanager

---
+++ Assets/Scripts/LivlNetworkRoomManager.cs
@@ -0,0 +1,20 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class LivlNetworkRoomManager : NetworkRoomManager
+{
+    public override void OnRoomServerDisconnect(NetworkConnectionToClient conn)
+    {
+        var playerStatsController = FindObjectOfType<PlayerStatsController>();
+
+        if (playerStatsController == null)
+        {
+            Debug.LogError("PlayerStatsController not found");
+            return;
+        }
+
+        playerStatsController.CmdRemovePlayer(conn.identity.netId);
+        
+        base.OnRoomServerDisconnect(conn);
+    }
+}
